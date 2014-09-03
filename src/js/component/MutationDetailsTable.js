c402b4fb102266d8eab4d94bdcb35f1b2c69099f
Eduardo Velasco <velasco810@gmail.com>
2014-09-03 18:17:54 +0000

    MutationDetailsTable.js edited online with Bitbucket

--- src/js/component/MutationDetailsTable.js
+++ src/js/component/MutationDetailsTable.js
@@ -841,7 +841,7 @@ function MutationDetailsTable(options, gene, mutationUtil)
 		// aoColumnDefs, oColVis, and fnDrawCallback may break column
 		// visibility, sorting, and filtering. Proceed wisely ;)
 		dataTableOpts: {
-			"sDom": '<"H"<"mutation_datatables_filter"f>C<"mutation_datatables_info"i>>t<"F">',
+			"sDom": 'T<"H"<"mutation_datatables_filter"f>C<"mutation_datatables_info"i>>t<"F">>',
 			"bJQueryUI": true,
 			"bPaginate": false,
 			//"sPaginationType": "two_button",
@@ -852,10 +852,15 @@ function MutationDetailsTable(options, gene, mutationUtil)
 				"sInfo": "Showing _TOTAL_ mutation(s)",
 				"sInfoFiltered": "(out of _MAX_ total mutations)",
 				"sInfoEmpty": "No mutations to show"
-			}
-		}
-	};
+			},
 
+      		 "oTableTools": {
+                 "aButtons": ["xls", "pdf"],
+                 "sSwfPath": "copy_cvs_xls_pdf.swf"
+         	}
+
+			}
+	};
 	// merge options with default options to use defaults for missing values
 	var _options = jQuery.extend(true, {}, _defaultOpts, options);
 
