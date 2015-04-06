2ade7b8d54cb74a9e2de4b55a56c95f2af03931a
Onur Sumer <onursumer@users.noreply.github.com>
2015-04-06 14:17:41 -0400

    changed the download button text to Download, and the extension to tsv

--- src/js/component/MutationDetailsTable.js
+++ src/js/component/MutationDetailsTable.js
@@ -1077,7 +1077,7 @@ function MutationDetailsTable(options, gene, mutationUtil, dataProxies)
 			"oTableTools": {
 				"aButtons": [{
 					"sExtends": "text",
-					"sButtonText": "CSV",
+					"sButtonText": "Download",
 					"mColumns": getExportColumns(columnOpts, excludedCols),
 					"fnCellRender": function(sValue, iColumn, nTr, iDataIndex) {
 						var value = sValue;
@@ -1102,7 +1102,7 @@ function MutationDetailsTable(options, gene, mutationUtil, dataProxies)
 						var content = this.fnGetTableData(oConfig);
 
 						var downloadOpts = {
-							filename: "mutation_table_" + gene + ".csv",
+							filename: "mutation_table_" + gene + ".tsv",
 							contentType: "text/plain;charset=utf-8",
 							preProcess: false};
 
