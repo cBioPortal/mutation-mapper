cc3b8e1dfdf4a50983f3f54235781fc7d150dba7
Onur Sumer <s.onur.sumer@gmail.com>
2016-11-28 14:54:51 -0500

    added MutationCollection to the export list

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,9 @@
-// TODO expose more classes?
+// TODO this is manual: not a desired way to expose sub-modules
 module.exports = {
 	MutationMapper: require("./MutationMapper"),
+	model: {
+		MutationCollection: require("./model/MutationCollection")
+	},
 	data: {
 		MutationDataProxy: require("./data/MutationDataProxy")
 	},
